543ef46f8579f3570bb55cf03ce60e8825a78973
KidChoque <lucassmachado1998@gmail.com>
2023-04-06 10:02:44 -0300

    Feito o exercício do dia 03-04-23

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿Console.WriteLine(@$"
+
+----------------------------------
+|  Bem vindo a nossa calculadora |
+|  operações:                    |
+|  Digite a opeção:              |
+| '+' adicao                     |
+| '*' multiplicacao              |
+| '-' subtracao                  |
+| '/' divisao                    |
+|--------------------------------|
+");
+
+char operation = char.Parse(Console.ReadLine());
+
+float resultado = 0;
+
+
+Console.WriteLine($"Digite o valor1");
+
+float valor1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite o valor2");
+
+float valor2 = float.Parse(Console.ReadLine());
+
+switch (operation)
+
+{case '+': resultado = ( valor1 + valor2);
+          Console.WriteLine($" O resultado da soma é {resultado} ");
+          break;
+
+case '*': resultado = ( valor1 * valor2 );
+          Console.WriteLine($" O resultado da multiplicação é {resultado} ");
+          break;
+
+case '-': resultado = ( valor1 - valor2 );
+          Console.WriteLine($" O resultado da subtração é {resultado} ");
+          break;
+
+case '/': resultado = ( valor1 / valor2 );
+          Console.WriteLine($" O resultado da divisão é {resultado} ");
+          break;
+
+          default: Console.WriteLine($" A operação informada não é suportada pela calculadora ! ");
+                   break;
+                   }
+                   
+                    
+   
